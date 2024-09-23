0ee2d068d02e7dbf4796f3b9c0f0719da0e36ffc
sunder-kirei <sunder-kirei@outlook.com>
2024-09-23 16:07:20 +0530

    changed fetch

--- lib/query/fetchRecent.ts
+++ lib/query/fetchRecent.ts
@@ -1,24 +1,27 @@
 import { Recent } from "@/types/api/recent";
 import { MetaProvider } from "../api/metaProvider";
-import { IAnimeInfo } from "@consumet/extensions";
 
 export async function fetchRecent() {
   const raw = await MetaProvider.fetchRecentEpisodes("gogoanime", 1, 10);
-  const promises: Promise<IAnimeInfo>[] = [];
-  raw.results.forEach((anime) => {
-    promises.push(MetaProvider.fetchAnilistInfoById(anime.id));
-  });
-  const info = await Promise.all(promises);
+  console.dir({ raw });
 
-  const data = raw.results.map((gogo) => {
-    const found = info.find((i) => i.id === gogo.id);
-    return {
-      ...gogo,
-      image: found?.image,
-      cover: found?.cover,
-      color: found?.color,
-    };
-  }) as Recent[];
+  const data = [] as Recent[];
 
+  for (const key in raw.results) {
+    const gogo = raw.results[key];
+    try {
+      const info = await MetaProvider.fetchAnilistInfoById(gogo.id);
+      data.push({
+        ...gogo,
+        image: info?.image || gogo.image || "",
+        cover: info?.cover || gogo.cover || "",
+        color: info?.color || gogo.color || "",
+      } as Recent);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      data.push({ ...gogo } as Recent);
+    }
+  }
+  console.dir({ data });
   return data;
 }

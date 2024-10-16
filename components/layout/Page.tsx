439a728b862c7c9b79b8758c421c7eccb5c229ad
sunder-kirei <sunder-kirei@outlook.com>
2024-10-16 23:52:10 +0530

    ui fix

--- components/layout/Page.tsx
+++ components/layout/Page.tsx
@@ -5,7 +5,7 @@ export function Page({ className, ...props }: HTMLAttributes<HTMLElement>) {
   return (
     <div
       className={twMerge(
-        "mx-auto h-fit min-h-screen w-full max-w-screen-2xl p-4 pb-20 sm:pb-4 sm:pl-20",
+        "mx-auto h-fit min-h-screen w-full max-w-screen-2xl p-4 pb-20 sm:p-0 sm:pb-4 sm:pl-20",
         className,
       )}
       {...props}

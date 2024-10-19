b36832de77cc66635624a398b41d921f539f22b0
sunder-kirei <sunder-kirei@outlook.com>
2024-10-19 22:24:46 +0530

    change history schema and ui

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,4 @@
 import { Page } from "@/components/layout/Page";
-import { createAvatar } from "@dicebear/core";
-import { adventurerNeutral } from "@dicebear/collection";
 import { genAvatar } from "@/lib/genAvatar";
 
 export default function ProfilePage() {

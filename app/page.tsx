0f679020a845405df920ee547a0955b7fb9ebbdb
sunder-kirei <sunder-kirei@outlook.com>
2024-10-19 09:55:16 +0530

    fix banner and recent cache issue

--- app/page.tsx
+++ app/page.tsx
@@ -17,56 +17,58 @@ export default async function Home() {
   const popular = await fetchPopular();
 
   return (
-    <Page className="sm:px-0 sm:py-0">
+    <>
       {trending.length && <RecentsCarousel recents={trending} />}
-      <main className="mt-4 flex flex-col gap-y-4 pb-20 pl-0 sm:pb-0 sm:pl-20">
-        {/* History */}
-        <HistorySection />
-        {/* Trending */}
-        <SectionHeading title="Recents" />
-        <Section className="flex-nowrap justify-start">
-          {recent.map((anime) => (
-            <Link key={anime.episodeId} href={`/anime/${anime.id}`}>
-              <div
-                className="relative aspect-video h-44 rounded bg-cover bg-center"
-                style={{
-                  backgroundImage: `url(${anime.image})`,
-                }}
-              >
+      <Page className="sm:py-0 sm:pl-0">
+        <main className="mt-4 flex flex-col gap-y-4 pb-20 pl-0 sm:pb-0 sm:pl-20">
+          {/* History */}
+          <HistorySection />
+          {/* Trending */}
+          <SectionHeading title="Recents" />
+          <Section className="flex-nowrap justify-start">
+            {recent.map((anime) => (
+              <Link key={anime.episodeId} href={`/anime/${anime.id}`}>
                 <div
-                  className="absolute left-0 top-0 h-full w-full"
+                  className="relative aspect-video h-44 rounded bg-cover bg-center"
                   style={{
-                    background: `linear-gradient(0deg, rgba(0,0,0,0.8016456582633054) 0%, rgba(255,255,255,0) 100%)`,
+                    backgroundImage: `url(${anime.image})`,
                   }}
                 >
-                  <div className="absolute bottom-0 flex h-16 w-full items-start gap-2 px-4 py-2">
-                    <div
-                      className="line-clamp-2 w-full overflow-ellipsis font-bold"
-                      style={{
-                        color: anime.color || "white",
-                      }}
-                    >
-                      {anime.title.english ||
-                        anime.title.romaji ||
-                        anime.title.native}
-                    </div>
-                    <div className="w-1/4 rounded bg-accent text-center font-semibold text-white">
-                      {anime.episodeNumber}
+                  <div
+                    className="absolute left-0 top-0 h-full w-full"
+                    style={{
+                      background: `linear-gradient(0deg, rgba(0,0,0,0.8016456582633054) 0%, rgba(255,255,255,0) 100%)`,
+                    }}
+                  >
+                    <div className="absolute bottom-0 flex h-16 w-full items-start gap-2 px-4 py-2">
+                      <div
+                        className="line-clamp-2 w-full overflow-ellipsis font-bold"
+                        style={{
+                          color: anime.color || "white",
+                        }}
+                      >
+                        {anime.title.english ||
+                          anime.title.romaji ||
+                          anime.title.native}
+                      </div>
+                      <div className="w-1/4 rounded bg-accent text-center font-semibold text-white">
+                        {anime.episodeNumber}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Link>
-          ))}
-        </Section>
-        {/* Popular */}
-        <SectionHeading title="Popular" />
-        <Section>
-          {popular.map((anime) => (
-            <AnimeCard anime={anime} key={anime.id} />
-          ))}
-        </Section>
-      </main>
-    </Page>
+              </Link>
+            ))}
+          </Section>
+          {/* Popular */}
+          <SectionHeading title="Popular" />
+          <Section>
+            {popular.map((anime) => (
+              <AnimeCard anime={anime} key={anime.id} />
+            ))}
+          </Section>
+        </main>
+      </Page>
+    </>
   );
 }

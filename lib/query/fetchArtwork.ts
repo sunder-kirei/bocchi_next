163dd56c4ccbf9ae22fbc31405d041ed2e10efd3
Sunder Kumar <sunder-kirei@outlook.com>
2025-02-13 16:48:09 +0530

    build fix

--- lib/query/fetchArtwork.ts
+++ lib/query/fetchArtwork.ts
@@ -3,6 +3,9 @@ import { Artwork } from "@/types/api/info";
 export async function fetchArtwork(id: string, page: number = 1) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/anime/artwork?id=${id}&page=${page}`,
+    {
+      cache: "no-store",
+    },
   );
   const json = await res.json();
   const data: Artwork[] = json.artwork;
